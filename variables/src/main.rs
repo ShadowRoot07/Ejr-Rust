b18e29ce17428fa37652267877f75fe247965e77
ShadowRoot07 <angstart.1109@gmail.com>
2026-07-10 22:37:50 +0000

    Feat: ejercicio variables corregido

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,20 +1,21 @@
 fn main() {
-    //demostracion de variables inmutables 
+    // Demostracion de variables inmutables
     let x: i32 = 10;
     println!("x: {x}");
-    // x = 20; //esto retorna errorr
+    // x = 20; // Esto retorna error
     // println!("x: {x}");
 
     //=================
-    //Demostracion de variables mutables 
-    mut y: i32 = 20;
-    println("y: {y}");
-
-    y = 22; //puede mutar 
-    println("y muto: {y}");
-
+    // Demostracion de variables mutablesi
     //=======================
-    // Enteros con signo:
+    let mut y: i32 = 20; 
+    println!("y: {y}");
+
+    y = 22; // Puede mutar
+    println!("y muto: {y}");
+    
+    //=======================
+    // Enteros con signo
     //=======================
     let ai: i8 = 1;
     let bi: i16 = 2;
@@ -22,30 +23,30 @@ fn main() {
     let di: i64 = 4;
     let ei: i128 = 5;
     let fi: isize = 6;  // isize y usize tienen el ancho de un puntero
-    
-    println("==============================");
-    println("Mostrando diferentes enteros con signo: ");
-    println("i8 = {ai}, i16 = {bi}");
-    println("i32 = {ci}, i64 = {di}");
-    println("i128 = {ei}, isize = {fi}");
-    println("==============================");
+
+    println!("==============================");
+    println!("Mostrando diferentes enteros con signo: ");
+    println!("i8 = {ai}, i16 = {bi}");
+    println!("i32 = {ci}, i64 = {di}");
+    println!("i128 = {ei}, isize = {fi}");
+    println!("==============================");
 
     //=======================
     // Enteros sin signo
     //=======================
-    let au: u8 = 1_000; // Esto favilita la lectura
+    let au: u8 = 1; 
     let bu: u16 = 2;
     let cu: u32 = 3;
     let du: u64 = 4;
-    let eu: u128 = 5;
-    let fu: usoze = 6;
+    let eu: u128 = 1_000;
+    let fu: usize = 6; 
 
-    println("==============================");
-    println("Mostrando diferentes enteros sin signo: ");
-    println("u8 = {au}, u16 = {bu}");
-    println("u8 = {au}, u16 = {bu}");
-    println("u8 = {au}, u16 = {bu}");
-    println("==============================");
+    println!("==============================");
+    println!("Mostrando diferentes enteros sin signo: ");
+    println!("u8 = {au}, u16 = {bu}");
+    println!("u32 = {cu}, u64 = {du}");
+    println!("u128 = {eu}, usize = {fu}");
+    println!("==============================");
 
     //=======================
     // Valores flotantes
@@ -53,31 +54,30 @@ fn main() {
     let af: f32 = 1.11;
     let bf: f64 = -2.15;
 
-    println("==============================");
-    println("Mostrandovloa dos valores tipo flotantes: ");
-    println("f32 = {af}, f63 = {bf}");
-    println("==============================");
+    println!("==============================");
+    println!("Mostrando los dos valores tipo flotantes: ");
+    println!("f32 = {af}, f64 = {bf}");
+    println!("==============================");
 
     //=======================
-    // Valores escaladores unicode 
+    // Valores escaladores unicode
     //=======================
-    let ac: char = "Hola"   // char tiene un tamaño de 32 bits
-    
-    println("==============================");
-    println("Mostrando valores escaladores: ");
-    println("char = {ac}");
-    println("==============================");
+    let astr: &str = "Hola";
+    let ac: char = "i";
+
+    println!("==============================");
+    println!("Mostrando valores escaladores: ");
+    println!("string = {astr}, char = {ac}");
+    println!("==============================");
 
     //=======================
-    // Valores Boleanos
+    // Valores Booleanos
     //=======================
-    
     let ab: bool = true;    // bool tiene 8 bits de ancho
     let bb: bool = false;
 
-    println("==============================");
-    println("Mostrando valores booleanos: ");   
-    println("true-bool = {ab}, false-bool = {bb}");
-    println("==============================");
-
+    println!("==============================");
+    println!("Mostrando valores booleanos: ");
+    println!("true-bool = {ab}, false-bool = {bb}");
+    println!("==============================");
 }
